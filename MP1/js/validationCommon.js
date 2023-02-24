fb8e382fcabbc993e5c4f33392b09de67678bb74
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-02-24 11:58:20 +0100

    Add form validation with javascript, add styling to error messages

--- MP1/js/validationCommon.js
+++ MP1/js/validationCommon.js
@@ -1,1 +1,56 @@
-﻿
+﻿function resetErrors(inputs, errorsTexts, errorInfo) {
+  for (let i = 0; i < inputs.length; i++) {
+    inputs[i].classList.remove("error-input");
+  }
+
+  for (let i = 0; i < errorsTexts.length; i++) {
+    errorsTexts[i].innerText = "";
+  }
+
+  errorInfo.innerText = "";
+}
+
+function checkRequired(value) {
+  if (!value) {
+    return false;
+  }
+
+  value = value.toString().trim();
+
+  if (value === "") {
+    return false;
+  }
+
+  return true;
+}
+
+function checkTextLengthRange(value, min, max) {
+  if (!value) {
+    return false;
+  }
+
+  value = value.toString().trim();
+  const length = value.length;
+
+  if (max && length > max) {
+    return false;
+  }
+
+  if (min && length < min) {
+    return false;
+  }
+
+  return true;
+}
+
+function checkEmail(value) {
+  if (!value) {
+    return false;
+  }
+
+  value = value.toString().trim();
+  const regex =
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+  return regex.test(value);
+}
--- MP1/js/validationEmpForm.js
+++ MP1/js/validationEmpForm.js
@@ -1,1 +1,58 @@
-﻿
+﻿function validateForm() {
+  const firstNameInput = document.getElementById("firstName");
+  const lastNameInput = document.getElementById("lastName");
+  const emailInput = document.getElementById("email");
+
+  const errorFirstName = document.getElementById("errorFirstName");
+  const errorLastName = document.getElementById("errorLastName");
+  const errorEmail = document.getElementById("errorEmail");
+  const errorsSummary = document.getElementById("errorsSummary");
+
+  resetErrors(
+    [firstNameInput, lastNameInput, emailInput],
+    [errorFirstName, errorLastName, errorEmail],
+    errorsSummary
+  );
+
+  let valid = true;
+
+  if (!checkRequired(firstNameInput.value)) {
+    valid = false;
+    firstNameInput.classList.add("error-input");
+    errorFirstName.innerText = "The field is required";
+  } else if (!checkTextLengthRange(firstNameInput.value, 2, 50)) {
+    valid = false;
+    firstNameInput.classList.add("error-input");
+    errorFirstName.innerText = "The field should contain 2 to 50 characters";
+  }
+
+  if (!checkRequired(lastNameInput.value)) {
+    valid = false;
+    lastNameInput.classList.add("error-input");
+    errorLastName.innerText = "The field is required.";
+  } else if (!checkTextLengthRange(lastNameInput.value, 2, 50)) {
+    valid = false;
+    lastNameInput.classList.add("error-input");
+    errorLastName.innerText = "The field should contain 2 to 50 characters.";
+  }
+
+  if (!checkRequired(emailInput.value)) {
+    valid = false;
+    emailInput.classList.add("error-input");
+    errorEmail.innerText = "The field is required.";
+  } else if (!checkTextLengthRange(emailInput.value, 5, 50)) {
+    valid = false;
+    emailInput.classList.add("error-input");
+    errorEmail.innerText = "The field should contain 5 to 50 characters.";
+  } else if (!checkEmail(emailInput.value)) {
+    valid = false;
+    emailInput.classList.add("error-input");
+    errorEmail.innerText = "The field should contain a valid email address.";
+  }
+
+  if (!valid) {
+    errorsSummary.innerText = "Form contains errors.";
+  }
+
+  return valid;
+}
