2f01d812de72c3d1785122a8d4097a0d7ce6f344
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-02-24 19:48:47 +0100

    Set up docker, install mysql2 and sequelize, set the connection parameters to the database and create the ORM mapper object

---
+++ MP2/tin-project-malinowski-s20824/config/sequelize/sequelize.js
@@ -0,0 +1,8 @@
+﻿const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("tin-project-sequelize", "root", "root", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
