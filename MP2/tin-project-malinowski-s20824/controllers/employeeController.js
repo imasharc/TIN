033ddcd415dbbef910ae1690d0e35bfa6117b28d
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-02-24 17:37:22 +0100

    Add controllers and initial routing for employees list and main page, create header and footer templates

---
+++ MP2/tin-project-malinowski-s20824/controllers/employeeController.js
@@ -0,0 +1,11 @@
+﻿exports.showEmployeeList = (req, res, next) => {
+  res.render("pages/employee/list", { navLocation: "emp" });
+};
+
+exports.showAddEmployeeForm = (req, res, next) => {
+  res.render("pages/employee/form", { navLocation: "emp" });
+};
+
+exports.showEmployeeDetails = (req, res, next) => {
+  res.render("pages/employee/details", { navLocation: "emp" });
+};
---
+++ MP2/tin-project-malinowski-s20824/routes/employeeRoute.js
@@ -0,0 +1,10 @@
+﻿const express = require("express");
+const router = express.Router();
+
+const employeeController = require("../controllers/employeeController");
+
+router.get("/", employeeController.showEmployeeList);
+router.get("/add", employeeController.showAddEmployeeForm);
+router.get("/details/:empId", employeeController.showEmployeeDetails);
+
+module.exports = router;
