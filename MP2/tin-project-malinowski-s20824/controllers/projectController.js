23abf7301dfa8eee21cdca73fff8caad18829d2f
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-02-26 20:56:35 +0100

    In the process of adding template for a universal form for project, UNFIXED BUG - 'Cannot read properties of null (reading 'projectId') at /views/pages/project_/form.ejs:8'

--- MP2/tin-project-malinowski-s20824/controllers/projectController.js
+++ MP2/tin-project-malinowski-s20824/controllers/projectController.js
@@ -1,13 +1,58 @@
-﻿exports.showProjectList = (req, res, next) => {
+﻿const EmployeeRepository = require("../repository/sequelize/EmployeeRepository");
+const ClientRepository = require("../repository/sequelize/ClientRepository");
+const ProjectRepository = require("../repository/sequelize/ProjectRepository");
+
+exports.showProjectList = (req, res, next) => {
   res.render("pages/project_/list", { navLocation: "projects" });
 };
 
 exports.showAddProjectForm = (req, res, next) => {
-  res.render("pages/project_/form", { navLocation: "projects" });
+  let allEmps, allClients;
+
+  EmployeeRepository.getEmployees()
+    .then((emps) => {
+      allEmps = emps;
+      return ClientRepository.getClients();
+    })
+    .then((clients) => {
+      allClients = clients;
+      res.render("pages/project_/form", {
+        project: {},
+        pageTitle: "New project",
+        formMode: "createNew",
+        btnLabel: "Add project",
+        formAction: "/project/add",
+        navLocation: "projects",
+        allEmps: allEmps,
+        allClients: allClients,
+      });
+    });
 };
 
 exports.showProjectDetails = (req, res, next) => {
-  res.render("pages/project_/details", { navLocation: "projects" });
+  const projectId = req.params.projectId;
+  let allEmps, allClients;
+
+  EmployeeRepository.getEmployees()
+    .then((emps) => {
+      allEmps = emps;
+      return ClientRepository.getClients();
+    })
+    .then((clients) => {
+      allClients = clients;
+      return ProjectRepository.getProjectById(projectId);
+    })
+    .then((project) => {
+      res.render("pages/project_/form", {
+        project: project,
+        pageTitle: "Show details",
+        formMode: "showDetails",
+        formAction: "",
+        navLocation: "projects",
+        allEmps: allEmps,
+        allClients: allClients,
+      });
+    });
 };
 
 exports.showEditProjectForm = (req, res, next) => {
