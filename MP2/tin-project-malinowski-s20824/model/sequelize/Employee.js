1396b0afd978b9c0cdc3c9c1493f3e76833a3d67
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-02-28 17:33:36 +0100

    Add partial validation for sequelize repostiries

--- MP2/tin-project-malinowski-s20824/model/sequelize/Employee.js
+++ MP2/tin-project-malinowski-s20824/model/sequelize/Employee.js
@@ -11,15 +11,45 @@ const Employee = sequelize.define("Employee", {
   firstName: {
     type: Sequelize.STRING(50),
     allowNull: false,
+    validate: {
+      nonEmpty: {
+        msg: "The field is required.",
+      },
+      len: {
+        args: [2, 50],
+        msg: "The field should contain between 2 and 50 characters.",
+      },
+    },
   },
   lastName: {
     type: Sequelize.STRING(50),
     allowNull: false,
+    validate: {
+      nonEmpty: {
+        msg: "The field is required.",
+      },
+      len: {
+        args: [2, 50],
+        msg: "The field should contain between 2 and 50 characters.",
+      },
+    },
   },
   email: {
     type: Sequelize.STRING(50),
     allowNull: false,
     unique: true,
+    validate: {
+      nonEmpty: {
+        msg: "The field is required.",
+      },
+      len: {
+        args: [2, 50],
+        msg: "The field should contain between 2 and 50 characters.",
+      },
+      isEmail: {
+        msg: "The field should contain a valid email address.",
+      },
+    },
   },
   role: {
     type: Sequelize.STRING(50),
