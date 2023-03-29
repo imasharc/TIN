195a65140b7779ad3d4ad0c88b1997da79afa1a4
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-29 20:49:46 +0200

    Calculate the n-th number of the Fibonacci sequence

---
+++ TIN-internet-based/materials/TIN_4/functions.js
@@ -0,0 +1,13 @@
+﻿function fibonacci(n) {
+  var num_1 = 0;
+  var num_2 = 1;
+  for (var i = 1; i < n; i++) {
+    var tmp = num_1 + num_2;
+    num_1 = num_2;
+    num_2 = tmp;
+  }
+  return num_1;
+}
+
+var n = 43;
+console.log(n + "th number of the Fibonacci sequence is " + fibonacci(n));
