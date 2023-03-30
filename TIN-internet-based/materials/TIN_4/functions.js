f3f349eca0ab532c24f063ba65cac34f81c880e1
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-30 11:20:52 +0200

    Check if provided string is a palindrome

--- TIN-internet-based/materials/TIN_4/functions.js
+++ TIN-internet-based/materials/TIN_4/functions.js
@@ -1,4 +1,5 @@
-﻿function fibonacci(n) {
+﻿// Calculate the n-th number of the Fibonacci sequence
+function fibonacci(n) {
   var num_1 = 0;
   var num_2 = 1;
   for (var i = 1; i < n; i++) {
@@ -11,3 +12,20 @@
 
 var n = 43;
 console.log(n + "th number of the Fibonacci sequence is " + fibonacci(n));
+
+// Check if the provided string is a palindrome
+function is_palindrome(txt) {
+  var reg = /[^A-Za-z0-9]/g;
+  txt = txt.toLowerCase().replace(reg, "");
+  var len = txt.length;
+
+  for (var i = 0; i < len / 2; i++) {
+    if (txt[i] !== txt[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+var txt = "A man, a plan, a canal. Panama";
+console.log(txt + " is a palindrome: " + is_palindrome(txt));
