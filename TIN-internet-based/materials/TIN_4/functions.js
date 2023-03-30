3c305357ff5374c76817e94c3cac2bd100dcba14
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-03-30 16:55:24 +0200

    Clean the code by deleting unnecessary lines and improving return statements

--- TIN-internet-based/materials/TIN_4/functions.js
+++ TIN-internet-based/materials/TIN_4/functions.js
@@ -7,75 +7,83 @@ function fibonacci(n) {
     num_1 = num_2;
     num_2 = tmp;
   }
-  return num_1;
+  return "Number " + n + " of the Fibonacci sequence is " + num_1 + "\n";
 }
 
-var n = 43;
-console.log(
-  "Number " + n + " of the Fibonacci sequence is " + fibonacci(n) + "\n"
-);
+console.log(fibonacci(6));
 
 // 2. Check if the provided string is a palindrome
-function is_palindrome(txt) {
+function is_palindrome(str) {
   var reg = /[^A-Za-z0-9]/g;
-  txt = txt.toLowerCase().replace(reg, "");
+  txt = str.toLowerCase().replace(reg, "");
   var len = txt.length;
 
   for (var i = 0; i < len / 2; i++) {
     if (txt[i] !== txt[len - 1 - i]) {
-      return false;
+      return "Provided string '" + str + "' is not a palindrome.\n";
     }
   }
-  return true;
+  return "Provided string '" + str + "' is a palindrome.\n";
 }
 
-var txt = "A man, a plan, a canal. Panama";
-console.log("Is '" + txt + "' a palindrome? " + is_palindrome(txt) + "\n");
+console.log(is_palindrome("Was it a car or a cat I saw?"));
 
 // 3. Determine the name of the type of the parameter provided to a function
-// TBA
+function determineType(param) {
+  // In JavaScript typeof null is an object but it is considered a bug. It should be null
+  if (param === null) {
+    return param + " is of type null";
+  } else if (param === undefined) {
+    return param + " is of type undefined";
+    // Becuase you cannot determine if a JavaScript object is an array (or a date) by using typeof
+    // therefore you may provide additional validation
+  } else if (param.constructor === Array) {
+    return param + " is of type Array(object)";
+  } else if (param.constructor === Date) {
+    return param + " is of type Date(object)";
+  }
+  return param + " is of type " + typeof param;
+}
+
+console.log(determineType() + "\n");
 
 // 4. Convert natural numbers into collection of 'coin' values
 function amountToCoins(num, coins) {
   var amount = [0, 0, 0, 0, 0];
-  if (num < 0) {
+  var tmp = num;
+  if (tmp < 0) {
     console.error("Number cannot be negative!");
     return -1;
   }
-  if (num - Math.floor(num) != 0) {
+  if (tmp - Math.floor(tmp) != 0) {
     console.error("You must provide a natural number!");
     return -1;
   }
-  while (num > 0) {
-    if (num - coins[0] > -1) {
+  while (tmp > 0) {
+    if (tmp - coins[0] > -1) {
       amount[0]++;
-      num = num - coins[0];
-    } else if (num - coins[1] > -1) {
+      tmp = tmp - coins[0];
+    } else if (tmp - coins[1] > -1) {
       amount[1]++;
-      num = num - coins[1];
-    } else if (num - coins[2] > -1) {
+      tmp = tmp - coins[1];
+    } else if (tmp - coins[2] > -1) {
       amount[2]++;
-      num = num - coins[2];
-    } else if (num - coins[3] > -1) {
+      tmp = tmp - coins[2];
+    } else if (tmp - coins[3] > -1) {
       amount[3]++;
-      num = num - coins[3];
-    } else if (num - coins[4] > -1) {
+      tmp = tmp - coins[3];
+    } else if (tmp - coins[4] > -1) {
       amount[4]++;
-      num = num - coins[4];
+      tmp = tmp - coins[4];
     }
-    return amount;
   }
-}
-
-var num = 46.54;
-if (amountToCoins(num, [25, 10, 5, 2, 1]) === -1) {
-  console.error("Some error occurred!");
-} else {
-  console.log(
+  return (
     num +
-      " can be represented as the following amount of coins " +
-      amountToCoins(num, [25, 10, 5, 2, 1]) +
-      " from the collection of " +
-      [25, 10, 5, 2, 1]
+    " can be represented as the following number of coins " +
+    amount +
+    " from the respectively ordered collection of " +
+    coins
   );
 }
+
+console.log(amountToCoins(46, [25, 10, 5, 2, 1]));
