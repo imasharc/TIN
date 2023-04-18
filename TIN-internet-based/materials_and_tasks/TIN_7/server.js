b3975e74337db2bb65deab7835eca695d12667c9
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-18 19:40:13 +0200

    Add target page that consumes the submitted data (/submit endpoint) and responds with the HTML showing the submitted data

---
+++ TIN-internet-based/materials_and_tasks/TIN_7/server.js
@@ -0,0 +1,40 @@
+﻿const http = require("http");
+
+// Create a server
+const server = http.createServer((req, res) => {
+  if (req.url === "/submit" && req.method === "POST") {
+    // Handle form submission
+    let data = "";
+    req.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    req.on("end", () => {
+      // Send response with submitted data
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Submitted Data</title>
+        </head>
+        <body>
+          <h1>Submitted Data</h1>
+          <p>Data: ${data}</p>
+        </body>
+      </head>
+      `);
+      res.end();
+    });
+  } else {
+    // Serve form page
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(require("fs").readFileSync("./form.html"));
+    res.end();
+  }
+});
+
+// Start the server on port 3000
+server.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
