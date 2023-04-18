29173436e5dc3861f0e5c5a52b2afdd0744e62b5
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-18 20:22:31 +0200

    Build a page with a form consisting of several fields, implement a target page for it that would construct a view using EJS including properly arranged data sent from that form, TO BE DONE - ADD SIMPLE EXTERNAL CSS

---
+++ TIN-internet-based/materials_and_tasks/TIN_7/task_3/app.js
@@ -0,0 +1,29 @@
+﻿const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+// Use body-parser middleware for parsing form data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set EJS as the view engine
+app.set("view engine", "ejs");
+
+// Serve static files from the public folder
+app.use(express.static("public"));
+
+// Render the form page
+app.get("/", (req, res) => {
+  res.render("form");
+});
+
+// Handle form submission
+app.post("/submit", (req, res) => {
+  const { firstName, lastName, email } = req.body;
+  const data = { firstName, lastName, email };
+  res.render("results", { data });
+});
+
+// Start the server on port 3000
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
