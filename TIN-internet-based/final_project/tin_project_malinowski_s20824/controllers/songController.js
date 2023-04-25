0d51d27d798e98f46329ed5c0b7cc61fad6e73d7
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-25 17:55:03 +0200

    Create controller for a 'song' table (TBA), add song-related functionalities to the controller

---
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/controllers/songController.js
@@ -0,0 +1,11 @@
+﻿exports.showSongList = (req, res, next) => {
+  res.render("pages/song/list", {});
+};
+
+exports.showAddSongForm = (req, res, next) => {
+  res.render("pages/song/form");
+};
+
+exports.showSongDetails = (req, res, next) => {
+  res.render("pages/song/details");
+};
