53891f2c570761a1da8421f0568df6e40077a572
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-25 19:42:17 +0200

    Add routing for the about page, correct fragments relative paths

--- TIN-internet-based/final_project/tin_project_malinowski_s20824/app.js
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/app.js
@@ -19,6 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use("/about", indexRouter);
 
 const songRouter = require("./routes/songRoute");
 app.use("/songs", songRouter);
