5b838a974b7e9b2f7c1598549d9a28051bf3b64c
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-26 20:13:13 +0200

    Create Sequelize data models - currently for Song only

---
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/model/sequelize/Song.js
@@ -0,0 +1,25 @@
+﻿const Sequelize = require("sequelize");
+const sequelize = require("../../config/sequelize/sequelize");
+
+const Song = sequelize.define("Song", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  url: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  isrc: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  },
+});
+
+module.exports = Song;
