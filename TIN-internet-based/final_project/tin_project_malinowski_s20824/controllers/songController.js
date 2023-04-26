b2cbdd90e00d7eafe9c2e593a4c137a092f4676c
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-26 10:01:08 +0200

    Use the feature of dynamic templates - pass parameters to the view from the controller level and based on them conditionally assign a class to the appropriate menu item

--- TIN-internet-based/final_project/tin_project_malinowski_s20824/controllers/songController.js
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/controllers/songController.js
@@ -1,11 +1,11 @@
 ﻿exports.showSongList = (req, res, next) => {
-  res.render("pages/song/list", {});
+  res.render("pages/song/list", { navLocation: "song" });
 };
 
 exports.showAddSongForm = (req, res, next) => {
-  res.render("pages/song/form");
+  res.render("pages/song/form", { navLocation: "song" });
 };
 
 exports.showSongDetails = (req, res, next) => {
-  res.render("pages/song/details");
+  res.render("pages/song/details", { navLocation: "song" });
 };
--- TIN-internet-based/final_project/tin_project_malinowski_s20824/routes/index.js
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { navLocation: "main" });
 });
 
 /* GET about page. */
 router.get("/about", function (req, res, next) {
-  res.render("about", { title: "Express" });
+  res.render("about", { navLocation: "about" });
 });
 
 module.exports = router;
