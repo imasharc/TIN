6bb0fb3a869fe7be4e7f833a1474dcd4f475405b
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-26 20:55:01 +0200

    Connect the new function call

--- TIN-internet-based/final_project/tin_project_malinowski_s20824/app.js
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/app.js
@@ -3,10 +3,13 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-
 var indexRouter = require("./routes/index");
-
 var app = express();
+
+const sequelizeInit = require("./config/sequelize/init");
+sequelizeInit().catch((err) => {
+  console.log(err);
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
