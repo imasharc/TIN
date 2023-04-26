c751bfde71c7452b0c1202a677f7f6ca6752d4ad
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-26 20:54:17 +0200

    Add init.js - force our model to synchronise with the database schema, conditionally insert sample data

---
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/config/sequelize/init.js
@@ -0,0 +1,33 @@
+﻿const sequelize = require("./sequelize");
+
+const Song = require("../../model/sequelize/Song");
+
+module.exports = () => {
+  let allSongs;
+
+  return sequelize
+    .sync({ force: true })
+    .then(() => {
+      return Song.findAll();
+    })
+    .then((songs) => {
+      if (!songs || songs.length == 0) {
+        return Song.bulkCreate([
+          {
+            title: "IS U CHEMICAL?",
+            url: "https://youtu.be/Lo6O4klwlDY",
+            isrc: "DEZC62331831",
+          },
+          {
+            title: "If I Die",
+            url: "https://youtu.be/979lWQefMno",
+            isrc: "QM8DG1874857",
+          },
+        ]).then(() => {
+          return Song.findAll();
+        });
+      } else {
+        return songs;
+      }
+    });
+};
