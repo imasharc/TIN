53aada213d27d3e6cbbd160cf2f402cffde03d3e
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-04-28 11:36:49 +0200

    Create a router for the new endpoints and connect it to the application

---
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/routes/api/SongApiRoute.js
@@ -0,0 +1,12 @@
+﻿const express = require("express");
+const router = express.Router();
+
+const songApiController = require("../../api/SongAPI");
+
+router.get("/", songApiController.getSongs);
+router.get("/:songId", songApiController.getSongById);
+router.post("/", songApiController.createSong);
+router.put("/:songId", songApiController.updateSong);
+router.delete("/:songId", songApiController.deleteSong);
+
+module.exports = router;
