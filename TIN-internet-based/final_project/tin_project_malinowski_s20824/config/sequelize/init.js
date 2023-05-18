c7864fd02a9ab016e9c10be09d6e9adb0c1aa342
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-05-18 09:01:09 +0200

    Create init.js file as the 'startup' for the application
    We define the relationship between the tables
    
    We will initialize a mapper instance that will be used in our repositories
    
    We will force our model to synchronize with the database schema (which may create tables)
    
    We will insert sample data (conditionally - if they do not already exist in the database)

--- TIN-internet-based/final_project/tin_project_malinowski_s20824/config/sequelize/init.js
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/config/sequelize/init.js
@@ -1,33 +1,153 @@
 ﻿const sequelize = require("./sequelize");
 
-const Song = require("../../model/sequelize/Song");
+const Engineer = require("../../model/sequelize/Engineer");
+const Studio = require("../../model/sequelize/Studio");
+const Booking = require("../../model/sequelize/Booking");
 
 module.exports = () => {
-  let allSongs;
+  Engineer.hasMany(Booking, {
+    as: "bookings",
+    foreignKey: { name: "engineerId", allowNull: false },
+    constraints: true,
+    onDelete: "CASCADE",
+  });
+  Booking.belongsTo(Engineer, {
+    as: "engineer",
+    foreignKey: { name: "engineerId", allowNull: false },
+  });
+  Studio.hasMany(Booking, {
+    as: "bookings",
+    foreignKey: { name: "studioId", allowNull: false },
+    constraints: true,
+    onDelete: "CASCADE",
+  });
+  Booking.belongsTo(Studio, {
+    as: "studio",
+    foreignKey: { name: "studioId", allowNull: false },
+  });
+
+  let allEngineers, allStudios;
 
   return sequelize
     .sync({ force: true })
     .then(() => {
-      return Song.findAll();
+      return Engineer.findAll();
     })
-    .then((songs) => {
-      if (!songs || songs.length == 0) {
-        return Song.bulkCreate([
+    .then((engineers) => {
+      if (!engineers || engineers.length == 0) {
+        return Engineer.bulkCreate([
           {
-            title: "IS U CHEMICAL?",
-            url: "https://youtu.be/Lo6O4klwlDY",
-            isrc: "DEZC62331831",
+            firstName: "Janusz",
+            lastName: "Walczuk",
+            specialisation: "Mixing engineer",
+            hourRate: 60,
+            contact: "janusz.walczuk@nobocoto.pl",
           },
           {
-            title: "If I Die",
-            url: "https://youtu.be/979lWQefMno",
-            isrc: "QM8DG1874857",
+            firstName: "Michał",
+            lastName: "Wróblewski",
+            specialisation: "Mixing engineer",
+            hourRate: 80,
+            contact: "wroobel@nobocoto.pl",
+          },
+          {
+            firstName: "Jonathan 'DJ Johny'",
+            lastName: "Łoś",
+            specialisation: "Mastering engineer",
+            hourRate: 120,
+            contact: "djjohny@nobocoto.pl",
           },
         ]).then(() => {
-          return Song.findAll();
+          return Engineer.findAll();
         });
       } else {
-        return songs;
+        return engineers;
+      }
+    })
+    .then((engineers) => {
+      allEngineers = engineers;
+      return Studio.findAll();
+    })
+    .then((studios) => {
+      if (!studios || studios.length == 0) {
+        return Studio.bulkCreate([
+          {
+            name: "Studio 1",
+            street: "Ludwika Rydygiera 8",
+            postalCode: "01-793",
+            city: "Warsaw",
+            hourRate: 80,
+            contact: "kontakt@nobocoto.pl",
+          },
+          {
+            name: "Studio A",
+            street: "gen. Sylwestra Kaliskiego 15A",
+            postalCode: "01-476",
+            city: "Warsaw",
+            hourRate: 60,
+            contact: "kontakt@nobocoto.pl",
+          },
+          {
+            name: "Studio B",
+            street: "gen. Sylwestra Kaliskiego 15A",
+            postalCode: "01-476",
+            city: "Warsaw",
+            hourRate: 30,
+            contact: "kontakt@nobocoto.pl",
+          },
+        ]).then(() => {
+          return Engineer.findAll();
+        });
+      } else {
+        return studios;
+      }
+    })
+    .then((studios) => {
+      allStudios = studios;
+      return Booking.findAll();
+    })
+    .then((bookings) => {
+      if (!bookings || bookings.length == 0) {
+        return Booking.bulkCreate([
+          {
+            startTime: "2023-01-12T20:00:00.000+01:00",
+            endTime: "2023-01-12T21:00:00.000+01:00",
+            firstName: "Mateusz",
+            lastName: "Karaś",
+            contact: "mateusz.karas@soldoutagency.com",
+            engineerId: allEngineers[2].id,
+            studioId: allStudios[0].id,
+          },
+          {
+            startTime: "2023-02-22T16:30:00.000+01:00",
+            endTime: "2023-02-22T18:30:00.000+01:00",
+            firstName: "Michał",
+            lastName: "Matczak",
+            contact: "mata@gmail.com",
+            engineerId: allEngineers[0].id,
+            studioId: allStudios[1].id,
+          },
+          {
+            startTime: "2023-04-07T19:00:00.000+01:00",
+            endTime: "2023-04-07T20:00:00.000+01:00",
+            firstName: "Janusz",
+            lastName: "Walczuk",
+            contact: "janusz.walczuk@nobocoto.pl",
+            engineerId: allEngineers[1].id,
+            studioId: allStudios[2].id,
+          },
+          {
+            startTime: "2023-04-11T18:00:00.000+01:00",
+            endTime: "2023-04-11T20:00:00.000+01:00",
+            firstName: "Adrian",
+            lastName: "Nowak",
+            contact: "adi.nowak@gmail.com",
+            engineerId: allEngineers[1].id,
+            studioId: allStudios[0].id,
+          },
+        ]);
+      } else {
+        return bookings;
       }
     });
 };
