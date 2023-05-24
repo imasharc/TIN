60682b2f4f08dfc512da69ee009eec9dbf47a8d9
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-05-24 18:54:59 +0200

    Create authController with functions login and logount
    Afte proper authentication, we save the logged-in users data in the session: req.session.loggedUser = emp;
    
    This way, on subsequent requests from this user, we will be able to recognize him/her and, for example, display his/hers data on the page, and protect our application from unauthorized use.
    
    The logout function removes the users data from the session.

---
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/controllers/authController.js
@@ -0,0 +1,34 @@
+﻿const EngineerRepository = require("../repository/sequelize/EngineerRepository");
+const authUtil = require("../utils/authUtils");
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  EngineerRepository.findByEmail(email)
+    .then((engineer) => {
+      if (!engineer) {
+        res.render("index", {
+          navLocation: "",
+          loginError: "Invalid email address or password",
+        });
+      } else if (
+        authUtil.comparePasswords(password, engineer.password) === true
+      ) {
+        req.session.loggedUser = engineer;
+        res.redirect("/");
+      } else {
+        res.render("index", {
+          navLocation: "",
+          loginError: "Invalid email address or password",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.logout = (req, res, next) => {
+  req.session.loggedUser = undefined;
+  res.redirect("/");
+};
