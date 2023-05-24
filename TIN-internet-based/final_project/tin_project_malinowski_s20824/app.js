ce3ea368738d839ec955a755495aab14aeaa96e5
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-05-24 18:44:27 +0200

    Update the app.js with sessions
    Sessions will be used to store data (on server-side) about the currently logged-in user

--- TIN-internet-based/final_project/tin_project_malinowski_s20824/app.js
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/app.js
@@ -4,26 +4,40 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var indexRouter = require("./routes/index");
-var bodyParser = require("body-parser");
 var app = express();
+const session = require("express-session");
+const authUtils = require("./utils/authUtils");
 app.use(express.json());
-// app.use(bodyParser.json());
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: false }));
-// app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: "my_secret_password",
+    resave: false,
+  })
+);
+
+app.use((req, res, next) => {
+  const loggedUser = req.session.loggedUser;
+  res.locals.loggedUser = loggedUser;
+  if (!res.locals.loginError) {
+    res.locals.loginError = undefined;
+  }
+  next();
+});
 
 app.use("/", indexRouter);
 app.use("/about", indexRouter);
 
 const engineerRouter = require("./routes/engineerRoute");
-app.use("/engineers", engineerRouter);
+app.use("/engineers", authUtils.permitAuthenticatedUser, engineerRouter);
 const bookingRouter = require("./routes/bookingRoute");
 app.use("/bookings", bookingRouter);
 const studioRouter = require("./routes/studioRoute");
