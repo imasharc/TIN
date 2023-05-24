2e16586fa572f145d0a9a158daef0be01ce0026b
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-05-24 18:55:38 +0200

    Connect the new features to the application.

--- TIN-internet-based/final_project/tin_project_malinowski_s20824/routes/index.js
+++ TIN-internet-based/final_project/tin_project_malinowski_s20824/routes/index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+const AuthController = require("../controllers/authController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -10,5 +11,8 @@ router.get("/", function (req, res, next) {
 router.get("/about", function (req, res, next) {
   res.render("about", { navLocation: "about" });
 });
+
+router.post("/login", AuthController.login);
+router.get("/logout", AuthController.logout);
 
 module.exports = router;
