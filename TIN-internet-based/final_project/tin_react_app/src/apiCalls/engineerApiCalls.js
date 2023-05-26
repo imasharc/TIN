f2a9b393083dd64509e71c9732c426d46f3f6476
amalinowski <antonimalinowski.kontakt@gmail.com>
2023-05-26 16:18:23 +0200

    In the engineereApiCalls.js file, define the appropriate data access functions

---
+++ TIN-internet-based/final_project/tin_react_app/src/apiCalls/engineerApiCalls.js
@@ -0,0 +1,12 @@
+﻿import { engineerList, engineerDetailsList } from "./engineerApiMockData";
+
+export function getEngineersApiCall() {
+  return engineerList;
+}
+
+export function getEngineerByIdApiCall(engineerId) {
+  const engineer = engineerDetailsList.find(
+    (engineer) => engineer.id === engineerId
+  );
+  return engineer;
+}
